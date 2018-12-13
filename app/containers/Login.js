2cea2c8655bbc95781b5f0794e5f95b429efbbdf
Tosiwe <tswxhcyspa@gmail.com>
2018-12-14 00:25:48 +0800

    update

--- app/containers/Login.js
+++ app/containers/Login.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react"
 import { StyleSheet, View, Image, TouchableOpacity, Text } from "react-native"
 import { connect } from "react-redux"
-import { InputItem,Button } from "antd-mobile-rn"
+import { InputItem, Button, List } from "antd-mobile-rn"
 import Icon from "react-native-vector-icons/AntDesign"
-
+import {primaryColor} from '../styles/common'
 import { createAction, NavigationActions } from "../utils"
 
 @connect(({ app }) => ({ ...app }))
@@ -26,14 +26,53 @@ class Login extends Component {
       <View style={styles.container}>
         <Text style={styles.logoText}>工基建</Text>
         <View>
-          <InputItem placeholder="手机号" style={styles.input}>
-            <Icon style={styles.inputIcon} name="phone" />
-          </InputItem>
-          <InputItem placeholder="密码" style={styles.input}>
-            <Icon style={styles.inputIcon} name="lock" />
-          </InputItem>
+          <List style={styles.list}>
+            <InputItem
+              clear
+              type="phone"
+              placeholder="手机号"
+              maxLength={11}
+              style={styles.input}
+            >
+              <Icon style={styles.inputIcon} name="phone" />
+            </InputItem>
+            <InputItem
+              clear
+              type="password"
+              placeholder="密码"
+              style={styles.input}
+            >
+              <Icon style={styles.inputIcon} name="lock" />
+            </InputItem>
+          </List>
         </View>
-        <Button size="large" type="primary">登录</Button>
+        <Button
+          size="large"
+          type="primary"
+          style={styles.loginBtn}
+          activeStyle={styles.activeLoginBtn}
+        >
+          登录
+        </Button>
+        <View style={styles.actions}>
+          <TouchableOpacity>
+            <Text style={{ color: primaryColor }}>注册账号</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={{ color: primaryColor }}>忘记密码</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.thirdLogin}>
+          <Text style={{color:'#666'}}>第三方登录</Text>
+          <View style={styles.thirdIconWrap}>
+            <TouchableOpacity>
+              <Icon name="wechat" style={styles.thirdIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="QQ" style={styles.thirdIcon} />
+            </TouchableOpacity>
+          </View>
+        </View>
         {!fetching && (
           <TouchableOpacity style={styles.close} onPress={this.onClose}>
             <Image
@@ -52,7 +91,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#3a3a3a",
+    backgroundColor: "#ddd",
     height: 20,
     borderBottomWidth: 0
   },
@@ -66,23 +105,51 @@ const styles = StyleSheet.create({
     color: "#f57b6a",
     marginBottom: 100
   },
+  loginBtn: {
+    marginTop: 30,
+    width: 300,
+    backgroundColor: "#00b100",
+    borderColor: "#00b100"
+  },
+  activeLoginBtn: {
+    backgroundColor: "#58b758"
+  },
 
   closeIcon: {
     width: 24,
     height: 24,
     tintColor: "gray"
   },
-  input: {
+  actions: {
+    marginVertical: 20,
     width: 300,
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    backgroundColor: "#3f3f3f",
-    color: "#7f7f7f"
+    justifyContent: "space-between",
+    flexDirection: "row"
+  },
+  list: {
+    borderRadius: 50,
+    width: 300
+  },
+  input: {
+    color: "#fff"
+  },
+  thirdLogin: {
+    alignItems: "center",
+    marginTop:30,
   },
   inputIcon: {
     color: "#7f7f7f",
     fontSize: 20,
     width: 20
+  },
+  thirdIcon: {
+    fontSize: 28,
+    marginTop: 20
+  },
+  thirdIconWrap:{
+    width:300,
+    flexDirection:'row',
+    justifyContent:'space-around'
   }
 })
 
