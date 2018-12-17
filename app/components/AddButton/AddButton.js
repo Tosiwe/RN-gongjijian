8b07685d3e49eb92d2b62972ce9babaa16829190
Tosiwe <tswxhcyspa@gmail.com>
2018-12-17 23:20:15 +0800

    adbtn

--- app/components/AddButton/AddButton.js
+++ app/components/AddButton/AddButton.js
@@ -6,10 +6,10 @@ import {
   Easing,
   View,
   StyleSheet,
-  Text,
+  Text
 } from "react-native"
 import { connect } from "react-redux"
-
+import { Modal, Button } from "antd-mobile-rn"
 // import { NavigationActions } from "react-navigation"
 
 import FAwesomeIcon from "react-native-vector-icons/FontAwesome"
@@ -24,248 +24,46 @@ import {
   bubbleColor,
   animateTime,
   easingType,
-  delay,
- 
+  delay
 } from "./styles"
 
 @connect(({ app }) => ({ ...app }))
 class AddButton extends Component {
   constructor(props) {
     super(props)
-    this.animatedValue = new Animated.Value(0)
-    this.topLeftValue = new Animated.Value(0)
-    this.topCenterValue = new Animated.Value(0)
-    this.topRightValue = new Animated.Value(0)
-    // this.state = {
-    //   pressed: false
-    // }
+    this.state = {
+      visible: false
+    }
   }
 
+  onClose = () => {
+    this.setState({
+      visible: false
+    })
+  };
+
   handleAddButtonPress = () => {
-	// const { pressed } = this.state
-	// if(pressed) {
-	// 	this.animateReverse(0)
-	// }
-	// else {
-	// 	this.animate(1)
-	// }
-	// this.setState({pressed: !pressed})
-   
-    // const { login } = this.props
-
-    // if (true) {
-	// 	this.props.dispatch(NavigationActions.navigate({ routeName: "Publish" }))
-	// } else {
-	// 	this.props.dispatch(NavigationActions.navigate({ routeName: "Login" }))
-    // }
-  };
-
-  animate = toValue => {
-    Animated.stagger(delay, [
-      Animated.parallel([
-        Animated.timing(this.animatedValue, {
-          toValue,
-          duration: animateTime,
-          easing: Easing.exp
-        }),
-        Animated.timing(this.topLeftValue, {
-          toValue,
-          duration: animateTime,
-          easing: easingType
-        })
-      ]),
-      Animated.timing(this.topRightValue, {
-        toValue,
-        duration: animateTime,
-        easing: easingType
-      })
-    ]).start()
-  };
-
-  animateReverse = toValue => {
-    Animated.stagger(delay, [
-      Animated.timing(this.topRightValue, {
-        toValue,
-        duration: animateTime,
-        easing: easingType
-      }),
-      Animated.timing(this.topCenterValue, {
-        toValue,
-        duration: animateTime,
-        easing: easingType
-      }),
-      Animated.parallel([
-        Animated.timing(this.animatedValue, {
-          toValue,
-          duration: animateTime,
-          easing: easingType
-        }),
-        Animated.timing(this.topLeftValue, {
-          toValue,
-          duration: animateTime,
-          easing: easingType
-        })
-      ])
-    ]).start()
+    Modal.operation([
+      { text: '标为未读', onPress: () => console.log('标为未读被点击了') },
+      { text: '置顶聊天', onPress: () => console.log('置顶聊天被点击了') },
+    ])
   };
 
   render() {
-    const springValue = Animated.add(
-      Animated.add(this.topLeftValue, this.topRightValue),
-      this.topCenterValue
-    )
     return (
-      <View >
-        <Animated.View
-          style={[
-            style.bigBubble,
-            {
-              transform: [
-                {
-                  rotateZ: springValue.interpolate({
-                    inputRange: [0, 1, 2, 3],
-                    outputRange: ["-45deg", "-45deg", "0deg", "45deg"]
-                  })
-                },
-                {
-                  scaleY: springValue.interpolate({
-                    inputRange: [0, 0.65, 1, 1.65, 2, 2.65, 3],
-                    outputRange: [1, 1.1, 1, 1.1, 1, 1.1, 1]
-                  })
-                }
-              ]
-            }
-          ]}
+      <View>
+        <TouchableOpacity
+          style={style.bigBubble}
+          hitSlop={{
+            left: 20,
+            right: 20,
+            top: 20,
+            bottom: 20
+          }}
+          onPress={this.handleAddButtonPress}
         >
-          <View
-		//   style={{ backgroundColor: "red", }}
-          // onStartShouldSetResponder={evt => {
-          // 	console.log("onStartShouldSetResponder")
-          // }}
-          // onMoveShouldSetResponder={evt=>{
-          // 	console.log("onMoveShouldSetResponder")
-          // }}
-          // onResponderGrant={evt=>{
-          // 	console.log("onResponderGrant")
-          // }}
-          // onResponderReject={evt=>{
-          // 	console.log("onResponderReject")
-          // }}
-          // onResponderMove={evt=>{
-          // 	console.log("onResponderMove")
-          // }}
-          // onResponderRelease={evt=>{
-          // 	console.log("onResponderRelease")
-          // }}
-          // onResponderTerminationRequest={evt=>{
-          // 	console.log("onResponderTerminationRequest")
-          // }}
-          // onResponderTerminate={evt=>{
-          // 	console.log("onResponderTerminate")
-          // }}
-          >
-            <TouchableOpacity
-              hitSlop={{
-                left: 20,
-                right: 20,
-                top: 20,
-                bottom: 20
-              }}
-              onPress={this.handleAddButtonPress}
-            >
-              <Animated.View
-                style={{
-                  transform: [
-                    {
-                      rotateZ: springValue.interpolate({
-                        inputRange: [0, 1, 2, 3],
-                        outputRange: ["45deg", "45deg", "45deg", "0deg"]
-                      })
-                    }
-                  ]
-                }}
-              >
-                <FAwesomeIcon name="plus" size={35} color="#FFF" />
-              </Animated.View>
-            </TouchableOpacity>
-          </View>
-        </Animated.View>
-        <Animated.View
-          style={[
-            style.smallBubble,
-            {
-              position: "absolute",
-              transform: [
-                {
-                  translateX: this.topLeftValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [center.left, topLeft.left]
-                  })
-                },
-                {
-                  translateY: this.topLeftValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [center.top, topLeft.top]
-                  })
-                },
-                {
-                  rotateZ: this.topLeftValue.interpolate({
-                    inputRange: [0, 0.6, 1],
-                    outputRange: ["-90deg", "-45deg", "0deg"]
-                  })
-                },
-                {
-                  scaleY: this.topLeftValue.interpolate({
-                    inputRange: [0, 0.8, 0.9, 1],
-                    outputRange: [1, 1.5, 1.5, 1]
-                  })
-                }
-              ],
-              opacity: this.topLeftValue,
-              zIndex: -1
-            }
-          ]}
-        >
-          <Text style={style.smallText}>发布需求</Text>
-        </Animated.View>
-        <Animated.View
-          style={[
-            style.smallBubble,
-            {
-              position: "absolute",
-              transform: [
-                {
-                  translateX: this.topRightValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [center.left, topRight.left]
-                  })
-                },
-                {
-                  translateY: this.topRightValue.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [center.top, topRight.top]
-                  })
-                },
-                {
-                  rotateZ: this.topRightValue.interpolate({
-                    inputRange: [0, 0.6, 1],
-                    outputRange: ["90deg", "45deg", "0deg"]
-                  })
-                },
-                {
-                  scaleY: this.topRightValue.interpolate({
-                    inputRange: [0, 0.8, 0.9, 1],
-                    outputRange: [1, 1.5, 1.5, 1]
-                  })
-                }
-              ],
-              opacity: this.topRightValue,
-              zIndex: -1
-            }
-          ]}
-        >
-          <Text style={style.smallText}>发布信息</Text>
-        </Animated.View>
+          <FAwesomeIcon name="plus" size={35} color="#FFF" />
+        </TouchableOpacity>
       </View>
     )
   }
@@ -280,17 +78,6 @@ const style = StyleSheet.create({
     width: bigBubbleSize,
     borderRadius: bigBubbleSize / 2,
     top: 3
-  },
-  smallBubble: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: bubbleColor,
-    height: smallBubbleSize,
-    width: smallBubbleSize * 2,
-    borderRadius: smallBubbleSize / 2
-  },
-  smallText: {
-    color: "#fff"
   }
 })
 
